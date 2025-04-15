70b74c64a07a1dd2fcb85fc389896b4d5c705b35
null1024-ws <shenwang918@gmail.com>
2025-04-15 13:32:43 +0800

    revise instrument script

--- instrument.sh
+++ instrument.sh
@@ -47,7 +47,7 @@ export LIBS="$LIBS -l:driver.o -lstdc++"
 )
 
 (
-	echo "## Build by Titan"
+	echo "## Build by Beacon"
     cd "$OUT"
     source "$TARGET/configrc"
     for p in "${PROGRAMS[@]}"; do (
@@ -60,22 +60,22 @@ export LIBS="$LIBS -l:driver.o -lstdc++"
 		extract-bc "./$p"
 
 		echo "[+] precondInfer"
-		$FUZZER/repo/prototype/precondInfer $p.bc --target-file=../cstest.txt --join-bound=1 > log_precond.txt 2>&1
+		$FUZZER/precondInfer $p.bc --target-file=../cstest.txt --join-bound=1 > log_precond.txt 2>&1
 		
 		echo "[+] Ins"
-		$FUZZER/repo/prototype/Ins -output=$folder/fuzz.bc -afl -res=$folder -log=log_Ins.txt -load=$folder/range_res.txt $folder/transed.bc
+		$FUZZER/Ins -output=$folder/fuzz.bc -afl -res=$folder -log=log_Ins.txt -load=$folder/range_res.txt $folder/transed.bc
 
 		echo "[+] Compile"
 		export CC=clang; export CXX=clang++;
 		input_bc=$folder/fuzz.bc
 		output_bin=$OUT/$p
-		afl_llvm_rt=$FUZZER/repo/prototype/afl-llvm-rt.o
-		export BUILD_BC_LIBS="$LIBS -lrt" 
+		afl_llvm_rt=$FUZZER/Test/afl-llvm-rt.o
+		export BUILD_BC_LIBS="$LIBS -lrt"
 		pushd "$TARGET/repo"
 		if [[ $TARGET = *libpng* ]]; then
 			$CXX $CXXFLAGS -std=c++11 -I. $input_bc -o $output_bin $afl_llvm_rt $LDFLAGS $BUILD_BC_LIBS .libs/libpng16.a -lz -lm
-		elif [[ $TARGET = *libsndfile* ]]; then
-			$CXX $CXXFLAGS -std=c++11 -I. $input_bc -o $output_bin $afl_llvm_rt $LDFLAGS $BUILD_BC_LIBS -lmp3lame -lmpg123 -lFLAC -lvorbis -lvorbisenc -lopus -logg -lm
+		# elif [[ $TARGET = *libsndfile* ]]; then
+		# 	$CXX $CXXFLAGS -std=c++11 -I. $input_bc -o $output_bin $afl_llvm_rt $LDFLAGS $BUILD_BC_LIBS -lmp3lame -lmpg123 -lFLAC -lvorbis -lvorbisenc -lopus -logg -lm
 		elif [[ $TARGET = *libtiff* ]]; then
 			WORK="$TARGET/work"
 			$CXX $CXXFLAGS -std=c++11 -I. $input_bc -o $output_bin $afl_llvm_rt $LDFLAGS $BUILD_BC_LIBS $WORK/lib/libtiffxx.a $WORK/lib/libtiff.a -lm -lz -ljpeg -Wl,-Bstatic -llzma -Wl,-Bdynamic
@@ -83,17 +83,17 @@ export LIBS="$LIBS -l:driver.o -lstdc++"
 			$CXX $CXXFLAGS -std=c++11 -I. $input_bc -o $output_bin $afl_llvm_rt $LDFLAGS $BUILD_BC_LIBS .libs/libxml2.a -lz -llzma -lm
 		elif [[ $TARGET = *lua* ]]; then
 			$CXX $CXXFLAGS -std=c++11 -I. $input_bc -o $output_bin $afl_llvm_rt $LDFLAGS $BUILD_BC_LIBS $TARGET/repo/liblua.a -DLUA_USE_LINUX -DLUA_USE_READLINE -lreadline -lm -ldl  # -L/$OUT 
-		elif [[ $TARGET = *openssl* ]]; then
-			$CXX $CXXFLAGS -std=c++11 -I. $input_bc -o $output_bin $afl_llvm_rt $LDFLAGS $BUILD_BC_LIBS -lpthread ./libcrypto.a ./libssl.a
-		elif [[ $TARGET = *php* ]]; then
-			$CXX $CXXFLAGS -std=c++11 -I. $input_bc -o $output_bin $afl_llvm_rt $LDFLAGS $BUILD_BC_LIBS -lstdc++ -lpthread -lboost_fiber -lboost_context
-		elif [[ $TARGET = *poppler* ]]; then
-			WORK="$TARGET/work"
-			$CXX $CXXFLAGS -std=c++11 -I. $input_bc -o $output_bin $afl_llvm_rt $LDFLAGS $BUILD_BC_LIBS -I"$WORK/poppler/cpp" -I"$TARGET/repo/cpp" \
-			"$WORK/poppler/cpp/libpoppler-cpp.a" "$WORK/poppler/libpoppler.a" "$WORK/lib/libfreetype.a" -lz -llzma -ljpeg -lz -lopenjp2 -lpng -ltiff -llcms2 -lm -lpthread -pthread
-		elif [[ $TARGET = *sqlite3* ]]; then
-                        WORK="$TARGET/work"
-			$CXX $CXXFLAGS -std=c++11 -I. $input_bc -o $output_bin $afl_llvm_rt $LDFLAGS $BUILD_BC_LIBS $WORK/.libs/libsqlite3.a -lpthread -pthread -ldl -lm -lz
+		# elif [[ $TARGET = *openssl* ]]; then
+		# 	$CXX $CXXFLAGS -std=c++11 -I. $input_bc -o $output_bin $afl_llvm_rt $LDFLAGS $BUILD_BC_LIBS -lpthread ./libcrypto.a ./libssl.a
+		# elif [[ $TARGET = *php* ]]; then
+		# 	$CXX $CXXFLAGS -std=c++11 -I. $input_bc -o $output_bin $afl_llvm_rt $LDFLAGS $BUILD_BC_LIBS -lstdc++ -lpthread -lboost_fiber -lboost_context
+		# elif [[ $TARGET = *poppler* ]]; then
+		# 	WORK="$TARGET/work"
+		# 	$CXX $CXXFLAGS -std=c++11 -I. $input_bc -o $output_bin $afl_llvm_rt $LDFLAGS $BUILD_BC_LIBS -I"$WORK/poppler/cpp" -I"$TARGET/repo/cpp" \
+		# 	"$WORK/poppler/cpp/libpoppler-cpp.a" "$WORK/poppler/libpoppler.a" "$WORK/lib/libfreetype.a" -lz -llzma -ljpeg -lz -lopenjp2 -lpng -ltiff -llcms2 -lm -lpthread -pthread
+		# elif [[ $TARGET = *sqlite3* ]]; then
+        #                 WORK="$TARGET/work"
+		# 	$CXX $CXXFLAGS -std=c++11 -I. $input_bc -o $output_bin $afl_llvm_rt $LDFLAGS $BUILD_BC_LIBS $WORK/.libs/libsqlite3.a -lpthread -pthread -ldl -lm -lz
 		else 
 			echo "Could not support this target $TARGET"
 		fi
