7dcbbe9746a14b4e3a48d82e0ec70641b7648a05
null024 <79917102+null1024-ws@users.noreply.github.com>
2025-04-16 14:57:50 +0800

    build driver.o

--- build.sh
+++ build.sh
@@ -15,4 +15,4 @@ set -e
 echo 'export LLVM_COMPILER=clang' >> "$HOME/.bashrc"
 echo 'export PATH="/usr/local/bin:$PATH"' >> "$HOME/.bashrc"
 source "$HOME/.bashrc"
-# wllvm++ $CXXFLAGS -std=c++11 -c "$FUZZER/repo/driver.cpp" -fPIC -o "$OUT/driver.o"
+wllvm++ $CXXFLAGS -std=c++11 -c "$FUZZER/repo/driver.cpp" -fPIC -o "$OUT/driver.o"
